e51c6ed71ed55adb6d42bb2289cae007579c03d9
Mohammad Nazir Sharifi <nazirsharifi19@gmail.com>
2023-03-30 22:58:12 +0300

    Uygulama Guncellendi:-> Dark Mode

--- src/data/applicationContext.js
+++ src/data/applicationContext.js
@@ -1,54 +1,70 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useState, useContext } from "react";
 import { LinkedList } from "../utilities/gameLogic";
 
 const initialAppStates = {
-    bg_image: {
-        value: "",
-        errorMessage: "",
-        isValid: undefined
-    },
-    gamme_linked_list: new LinkedList(),
-    current_screen: null
+  bg_image: {
+    value: "",
+    errorMessage: "",
+    isValid: undefined,
+  },
+  gamme_linked_list: new LinkedList(),
+  current_screen: null,
 };
 
 export const CreateAppContext = createContext({
-    ...initialAppStates,
-    handleFieldChange: () => { }
+  ...initialAppStates,
+  handleFieldChange: () => {},
 });
 
 const createAppContextReducer = (state, action) => {
-    switch (action.type) {
-        case "handleFieldChange": {
-            if (action.payload.name == "bg_image") {
-                return {
-                    ...state,
-                    bg_image: {
-                        value: action.payload.value,
-                        errorMessage: "",
-                        isValid: undefined
-                    }
-                };
-            }else return ({ ...state, [action.payload.name]: action.payload.value});
-        }
-        default: return state;
+  switch (action.type) {
+    case "handleFieldChange": {
+      if (action.payload.name == "bg_image") {
+        return {
+          ...state,
+          bg_image: {
+            value: action.payload.value,
+            errorMessage: "",
+            isValid: undefined,
+          },
+        };
+      } else return { ...state, [action.payload.name]: action.payload.value };
     }
+    default:
+      return state;
+  }
 };
 
 const CreateAppContextProvider = ({ children }) => {
+  const [appState, dispatch] = useReducer(
+    createAppContextReducer,
+    initialAppStates
+  );
+  const [darkTheme, setDarkTheme] = useState(true);
 
-    const [appState, dispatch] = useReducer(createAppContextReducer, initialAppStates);
+  const handleFieldChange = (name, value) => {
+    dispatch({ type: "handleFieldChange", payload: { name, value } });
+  };
 
-    const handleFieldChange = (name, value) => {
-        dispatch({ type: "handleFieldChange", payload: { name, value } });
-    };
+  const changeTheme = () => {
+    setDarkTheme(!darkTheme);
+  };
+  const value = {
+    ...appState,
+    handleFieldChange,
+    darkTheme,
+    changeTheme,
+  };
 
-    const value = {
-        ...appState, handleFieldChange
-    };
+  return (
+    <CreateAppContext.Provider value={value}>
+      {children}
+    </CreateAppContext.Provider>
+  );
+};
 
-    return <CreateAppContext.Provider value={value} >
-        {children}
-    </CreateAppContext.Provider>;
+export const useGlobalContext = () => {
+  return useContext(CreateAppContext);
 };
 
 export default CreateAppContextProvider;
